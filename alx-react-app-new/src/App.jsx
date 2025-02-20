8d998a6beebde0d8ec2950b9cdbc67647c009477
Mwamachi95 <mwamachi.mwamburi@gmail.com>
2025-02-20 11:18:36 +0300

    Commit new Counter.jsx file and update App.jsx to main

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,7 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'  
+import UserProfile from './components/UserProfile' 
+import Counter from './components/Counter' 
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,6 +39,7 @@ function App() {
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
+      <Counter />
     </>
   )
 }
