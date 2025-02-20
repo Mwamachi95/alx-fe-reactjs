37f853f4ac5755394f1df412f9f0d8c4a7bff40d
Mwamachi95 <mwamachi.mwamburi@gmail.com>
2025-02-20 11:12:11 +0300

    Commit files to main

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{textAlign: 'center', margin: '10px', padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: 'lightgray', padding: '10px', margin: '10px', textAlign: 'center' }}>
+            <p style={{ color: '#333', fontSize: '1.1rem' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
