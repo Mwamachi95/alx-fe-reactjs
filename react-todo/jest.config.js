c1e4787732de700f57e3d506008cc853d10d7c8b
Mwamachi95 <mwamachi.mwamburi@gmail.com>
2025-03-07 13:52:22 +0300

    Commit

--- react-todo/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],  // Ensures Jest picks up test files
-};
