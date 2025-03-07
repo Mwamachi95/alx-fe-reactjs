41eebf6a9cd4188298bb52a8cc7a9ea9a6fbd01f
Mwamachi95 <mwamachi.mwamburi@gmail.com>
2025-03-07 15:29:09 +0300

    Commit changes to main

--- react-todo/src/__tests__/AddTodoForm.test.js
+++ react-todo/src/__tests__/AddTodoForm.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import AddTodoForm from '../src/components/AddTodoForm';
+import AddTodoForm from '../components/AddTodoForm';
 
 describe('AddTodoForm Component', () => {
   test('renders the form correctly', () => {
