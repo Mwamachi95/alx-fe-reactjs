a794cab767f9f2c8ea6beed1406d8ecf632b5bcb
Mwamachi95 <mwamachi.mwamburi@gmail.com>
2025-03-26 21:30:42 +0300

    Added tailwind files

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust based on your file types and structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
